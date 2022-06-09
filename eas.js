bdda250a3bdad8a4a01f6003008160ab849ab8d5
max <maxklundt@web.de>
2022-06-09 07:34:28 +0200

    added h1 changed color and font-family

--- eas.js
+++ eas.js
@@ -1,19 +1,35 @@
+let userRows = 0;
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function(e) {
+  document
+    .querySelectorAll(".grid-item")
+    .forEach((e) => e.parentNode.removeChild(e));
+
+  canvasSize = prompt("How many rows?");
+  while (canvasSize > 100) {
+    canvasSize = prompt("How many rows?");
+  }
+  makeRows(canvasSize);
+})
+
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
+function makeRows(_canvasSize) {
+  const rows = canvasSize;
+  const cols = canvasSize;
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
+    cell.addEventListener("mouseover", function(e) {
+      e.target.style.backgroundColor = "white";
+      setTimeout(function() {
+        e.target.style.backgroundColor = "";
+      }, 300);
+    }, false);
   };
 };
-container.addEventListener("mouseover", function(e) {
-  e.target.style.backgroundColor = "red";
-  setTimeout(function() {
-    e.target.style.backgroundColor = "";
-  }, 300);
-}, false);
 
-makeRows(16, 16);
+
